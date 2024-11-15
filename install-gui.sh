0593a6ff3d198fea51b7bad4b8796c61400e54f9
ddtrong <148517918+ddtrong@users.noreply.github.com>
2024-11-15 07:34:36 +0700

    Update install-gui.sh to download and extract firefox in home

--- install-gui.sh
+++ install-gui.sh
@@ -6,3 +6,6 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome-remote-desktop*
 sudo apt --fix-broken install --assume-yes
 rm chrome-remote-desktop*
+rm -rf firefox firefox.tbz
+wget -o firefox.tbz https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US
+tar xf firefox.tbz
