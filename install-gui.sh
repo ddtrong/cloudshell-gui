94833a870ef13c3587722a20836f841055f07521
ddtrong <148517918+ddtrong@users.noreply.github.com>
2024-11-15 07:24:36 +0700

    Update install-gui.sh for Ubuntu 24.04

--- install-gui.sh
+++ install-gui.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 sudo apt update
-sudo apt install xvfb xfce4 xfce4-goodies mpv kdenlive simplescreenrecorder firefox-esr plank papirus-icon-theme dbus-x11 neofetch krita --assume-yes
+sudo apt install xvfb xfce4 xserver-xorg-video-dummy policykit-1 xbase-clients python3-psutil python3-xdg libgbm1 libutempter0 dbus-x11 --assume-yes
 # Download Chrome Remote Desktop, install it, then delete downloaded .deb package
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome-remote-desktop*
 sudo apt --fix-broken install --assume-yes
 rm chrome-remote-desktop*
-# Install Catppuccin theme
-sudo apt install sassc --assume-yes
-git clone https://github.com/catppuccin/gtk.git
-cd gtk
-sudo make build
-sudo make package
-cd pkgs
-sudo cp * /usr/share/themes
-cd /usr/share/themes
-~/install-catppuccin.sh
-# Install Catppuccin Plank theme
-cd
-git clone https://github.com/catppuccin/plank.git
-cd plank
-sudo cp -r Catppuccin /usr/share/plank/themes
-sudo cp -r Catppuccin-solid /usr/share/plank/themes
